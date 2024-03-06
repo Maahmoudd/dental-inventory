9b98607d913e997e0a85968ccd257792615966d0
Maahmoudd <mahmoudmuhammed2610@gmail.com>
2024-03-06 07:00:59 +0200

    Design dashboard layouts

---
+++ resources/views/backend/layouts/header.blade.php
@@ -0,0 +1,7 @@
+<div id="kt_header" style="" class="header align-items-stretch">
+    <!--begin::Container-->
+    <div class="container-fluid d-flex align-items-stretch justify-content-between">
+
+    </div>
+    <!--end::Container-->
+</div>
